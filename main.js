de7bb52c4661374a9731499245eb742990ac1f42
Atakan Ertek <faertek123@gmail.com>
2023-01-09 08:14:44 +0300

    main.js updated and korean foods added

---
+++ main.js
@@ -0,0 +1,75 @@
+const menu = [
+    {
+        id: 1,
+        title: "Tteokbokki", 
+        category: "Korea",
+        price: 8000,
+        currency: "₩",
+        img: "",
+        desc: "Spicy rice cakes, serving with fish cake.",
+    },
+    {
+        id: 2,
+        title: "Chicken Ramen", 
+        category: "Korea",
+        price: 7600,
+        currency: "₩",
+        img: "",
+        desc: "Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg."
+    },
+    {
+        id: 3,
+        title: "Bibimbap", 
+        category: "Korea",
+        price: 15000,
+        currency: "₩",
+        img: "",
+        desc: "Boiling vegetables, serving with special hot sauce."
+    },
+    {
+        id: 4,
+        title: "Jajangmyeon", 
+        category: "Korea",
+        price: 3800,
+        currency: "₩",
+        img: "",
+        desc: "Black bean sauce noodle, serving with green onion."
+    },
+    {
+        id: 5,
+        title: "Kimchi", 
+        category: "Korea",
+        price: 11200,
+        currency: "₩",
+        img: "",
+        desc: "A spicy and sour dish made up of fermented vegetables."
+    },
+    {
+        id: 6,
+        title: "Samgyetang", 
+        category: "Korea",
+        price: 15000,
+        currency: "₩",
+        img: "",
+        desc: "A common dish particularly during the summer, samgyetang is a traditional soup made of chicken, garlic, rice, scallion, Korean jujube, Korean ginseng, and spices."
+    },
+    {
+        id: 7,
+        title: "Samgyeopsal", 
+        category: "Korea",
+        price: 16000,
+        currency: "₩",
+        img: "",
+        desc: "Consists of grilled slices of pork belly meat that are not marinated or seasoned."
+    },
+    {
+        id: 8,
+        title: "Peking Roast Duck", 
+        category: "China",
+        price: 200,
+        currency: "¥",
+        img: "",
+        desc: "Consists of grilled slices of pork belly meat that are not marinated or seasoned."
+    },
+
+];
